50872174e835b98a49de5d2c3602ea22e81bf024
Nelly Khmeleva <nkhmeleva@yahoo.com>
2019-04-10 04:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function addToCart(item) {
 
 function viewCart(){
     if (cart.length===0) {
-      return("Your shopping cart is empty.");
+      console.log("Your shopping cart is empty.");
     } else {
       if (cart.length===1) {
       return(`In your cart, you have ${item} at ${price}.`);
